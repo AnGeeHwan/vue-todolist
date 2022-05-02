cb8b3acae7937860f2d593b303ff73f4bf21265e
AnGeeHwan <rlghks9252@gmail.com>
2022-05-02 23:06:33 +0900

    vue-todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -30,9 +30,9 @@
         </button>
         <ul class="dropdown-menu dropdown-menu-end">
           <li v-for="item in menu" :key="item.str">
-            <a class="dropdown-item" @click="item.func(todo.id)">
-              {{ item.str }}
-            </a>
+            <a class="dropdown-item" @click="item.func(todo.id)">{{
+              item.str
+            }}</a>
           </li>
         </ul>
       </div>
@@ -49,25 +49,24 @@ export default {
 
 <script setup>
 import { inject } from 'vue'
-
 const props = defineProps({
   data: {
     type: Array,
     default: [],
-  }
+  },
 })
 
-const removeTodo = inject("removeTodo")
+const removeTodo = inject('removeTodo')
 const completeTodo = inject('completeTodo')
-const today = inject("today")
+const today = inject('today')
 const menu = [
-   {
-        str: '할일 삭제',
-        func: removeTodo,
-      },
-      {
-        str: '할일 완료',
-        func: completeTodo,
-      },
+  {
+    str: '할일 삭제',
+    func: removeTodo,
+  },
+  {
+    str: '할일 완료',
+    func: completeTodo,
+  },
 ]
 </script>
